8174820dd0e41a8569ef149ee6744228c4276194
BrandonGYP <100327399+BrandonGYP@users.noreply.github.com>
2022-05-25 11:26:07 -0500

    Add files via upload

--- Dentista.cpp
+++ Dentista.cpp
@@ -1,22 +1,19 @@
 // Dentista.cpp
 #include <iostream>
+#include<string>
 #include<cstring>
 
 using namespace std;
 
-struct paciente
-{
-    int numss;
-    string nombre;
-    int edad;
-    string clinica;
-};
 
 struct citas
 {
-    int numcita;
-    int nss;
-    string especialista;
+    int cant;
+    int prest_unit = 100;
+    string hora;
+    string trata;
+    string nombre;
+    string tomar;
     string motivo;
 
 };
@@ -25,20 +22,17 @@ struct citas
 
 int main()
 {
-    paciente pacientes[2];
-    citas cita[2];
+
+    citas cita[10];
     int opcion;
     int i;
     int j;
     int op2;
     int op3;
-
-
-    string nombre, trata;
-    string hora, motiv;
-    int prest, pres_unit, total;
-
-
+    double resultado = 0;
+    double iva;
+    string a;
+    int t = 0;
     do
     {
         cout << "\n Menu de Opciones" << endl;
@@ -48,75 +42,107 @@ int main()
         cout << "4.Lista de Citas Vigentes" << endl;
         cout << "5.Limpiar Pantalla" << endl;
         cout << "6.Salir " << endl;
-
         cin >> opcion;
 
         switch (opcion)
         {
         case 1:
-            cout << "\nEscriba su nombre\n";
-            cin >> nombre;
-            cout << "\nA que hora acudira a su cita\n";
-            cin >> hora; 
-            cout << "\nQue tratamiento desea tomar\n";
-            cin >> trata;
-            cout << "\nCual es su motivo para tomar el tratamiento\n";
-            cin >> motiv;
-            cout << "\nPrecio Unitario del tratamiento\n";
-            cin >> prest;
-            cout << "\nPrecio Unitario\n";
-            cin >> pres_unit; 
+            cout << "NUMERO DE CITA" << t + 1 << endl;
+            cout << "ingrese su nombre" << endl;
+            getline(cin, cita[t].nombre);
+            getline(cin, cita[t].nombre);
+            cout << "A que hora acudira a su cita" << endl;
+            getline(cin, cita[t].hora);
+            cout << "Que tratamiento desea tomar" << endl;
+            getline(cin, cita[t].tomar);
+            cout << "Motivo para tomar su tratamiento" << endl;
+            getline(cin, cita[t].motivo);
+            cout << "Cantidad del tratamiento a tomar: " << endl;
+            cin >> cita[t].cant;
 
-            total = prest;
-            cout << "\nEl precio total es:\n" << total << endl;
+            resultado = cita[t].cant * cita[t].prest_unit;
+            iva = resultado * 0.16;
+            resultado = iva + resultado;
 
-            cout << "\nEl paciente: " << nombre << " genero una cita para las horas: " << hora << " para que se le ingrese el tratamiento de: " << trata << endl;
+            cout << "El precio toal es: " << resultado << endl;
+
+            cout << "El paciente: " << cita[t].nombre << " genero una cita para las horas: " << cita[t].hora << " para que se le ingrese el tratamiento de: " << cita[t].tomar << " por: " << cita[t].motivo << endl;
+            t++;
             break;
         case 2:
-            printf("numero de cita que desea modificar: \n");
+            printf("Numero de cita que desea modificar: ");
             cin >> j;
             j = j - 1; //1-1=0
-            printf("1.-Nombre \n 2.Hora de Cita \n 3.-Tratamiento a tomar\n");
+            printf("\n1.- Nombre \n 2.- Hora \n 3.- Tratamiento \n 4.- Motivo \n");
             cin >> op2;
+            getline(cin, a);
             switch (op2)
             {
             case 1:
                 for (i = j;i <= j;i++)
                 {
-                    cout << "ingrese el Nombre" << endl;
-                    cin >> pacientes[i].nombre;
+                    cout << "NUMERO DE CITA" << i + 1 << endl;
+                    cout << "ingrese el nombre: " << endl;
+                    getline(cin, cita[i].nombre);
+
                 }
                 break;
+
             case 2:
                 for (i = j;i <= j;i++)
                 {
-                    cout << "Ingrese hora de cita" << endl;
-                    cin >> pacientes[i].edad;
+                    cout << "A que hora desea cambiar su cita : " << endl;
+                    getline(cin, cita[i].hora);
+
                 }
                 break;
+
             case 3:
                 for (i = j;i <= j;i++)
                 {
-                    cout << "ingrese tratamiento a tomar" << endl;
-                    cin >> pacientes[i].clinica;
+                    cout << "Ingrese de nuevo el tratamiento que desea tomar: " << endl;
+                    getline(cin, cita[i].tomar);
+
+                }
+                break;
+
+            case 4:
+                for (i = j;i <= j;i++)
+                {
+                    cout << "Ingrese el motivo del tratamiento: " << endl;
+                    getline(cin, cita[i].motivo);
+
                 }
                 break;
             }
             break;
 
         case 3:
-           
-            break;
-        case 4:
+
             for (i = 0;i < 2;i++)
             {
-                cout << "numero cita: " << i + 1 << endl;
-                cout << cita[i].nss << endl;
-                cout << cita[i].especialista << endl;
+                printf("Numero de cita que desea eliminar: \n");
+                cin >> j;
+                j = j - 1;
+                for (i = j;i <= j;i++)
+                {
+                    cita[i].nombre = "";
+                    cita[i].hora = "";
+                    cita[i].tomar = "";
+                    cita[i].motivo = "";
+                }
+            }
+            break;
+        case 4:
+            for (i = 0;i < t;i++)
+            {
+                cout << "Numero cita: \n" << i + 1 << endl;
+                cout << cita[i].nombre << endl;
+                cout << cita[i].hora << endl;
+                cout << cita[i].tomar << endl;
                 cout << cita[i].motivo << endl;
 
             }
-            break;
             break;
 
         case 5:
